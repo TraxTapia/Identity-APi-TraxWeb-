95e469a5b326c478eed3fe97511d1999fe585db1
Ivan Tapia Avalos <ivan.tapia@mediaccess.com.mx>
2023-02-22 17:36:46 -0600

    bin

---
+++ Trax.Models.Generic/Api/ClientesDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trax.Models.Generic.Api
+{
+   
+    public partial class ClientesDTO
+    {
+        public int id_cliente { get; set; }
+     
+        public string nombre_cliente { get; set; }
+ 
+        public string email_cliente { get; set; }
+    
+        public string telefono_cliente { get; set; }
+  
+        public DateTime fecha_registro { get; set; }
+
+       
+    }
+}
---
+++ Trax.Models.Generic/Api/Response/ClientesListResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trax.Models.Generic.Api.Response
+{
+    public class ClientesListResponseDTO
+    {
+        public ClientesListResponseDTO()
+        {
+            this.Result = new OperationResult.OperationResult();
+        }
+        public List<ClientesDTO> List { get; set; }
+        public OperationResult.OperationResult Result { get; set; }
+    }
+}
