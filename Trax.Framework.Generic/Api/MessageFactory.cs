9f6b71daae790722a62d6f48346c44b87bde0cab
Ivan Tapia Avalos <ivan.tapia@mediaccess.com.mx>
2023-02-23 11:12:28 -0600

    cambios en modelo

---
+++ Trax.Framework.Generic/Api/MessageFactory.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Trax.Models.Generic.Identity.Response;
+
+namespace Trax.Framework.Generic.Api
+{
+    public class MessageFactory
+    {
+        private Logger.Logger _Logger;
+
+        public MessageFactory(Logger.Logger Logger)
+        {
+            this._Logger = Logger;
+        }
+        public T SendRequest<T>(string EndPointUrl, string Token, string Action, string Payload, HttpMethod Method)
+        {
+            string _Response = string.Empty;
+            DateTime dt1 = DateTime.Now;
+            for (int i = 0; i < 4; i++)
+            {
+                System.Net.ServicePointManager.Expect100Continue = false;
+                System.Net.ServicePointManager.DefaultConnectionLimit = 9999;
+                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Ssl3;
+                using (HttpClient _HttpClient = new HttpClient())
+                using (HttpRequestMessage _HttpRequestMessage = new HttpRequestMessage(Method, EndPointUrl + Action))
+                {
+                    _HttpClient.DefaultRequestHeaders.Clear();
+                    _HttpClient.MaxResponseContentBufferSize = 2147483647;
+                    _HttpClient.Timeout = TimeSpan.FromMilliseconds(5400000);
+                    _HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+                    _HttpRequestMessage.Content = new StringContent(Payload, Encoding.UTF8, "application/json");
+                    using (HttpResponseMessage _HttpResponseMessage = _HttpClient.SendAsync(_HttpRequestMessage).Result)
+                    {
+                        if (!_HttpResponseMessage.IsSuccessStatusCode)
+                        {
+                            switch (_HttpResponseMessage.StatusCode)
+                            {
+                                case System.Net.HttpStatusCode.BadRequest:
+                                    _Logger.LogText(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                    _Logger.LogText(EndPointUrl + Action);
+                                    _Logger.LogText(Payload);
+                                    throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                case System.Net.HttpStatusCode.Unauthorized:
+                                case System.Net.HttpStatusCode.NotFound:
+                                    throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                default:
+                                    TimeSpan span = (DateTime.Now) - dt1;
+                                    _Logger.LogText("Execution time: " + span.TotalMilliseconds.ToString() + " milliseconds, retried " + i.ToString() + " times.");
+                                    _Logger.LogText(EndPointUrl + Action);
+                                    _Logger.LogText(Payload);
+                                    if (i >= 3)
+                                    {
+                                        _Logger.LogText(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                        _Logger.LogText(EndPointUrl + Action);
+                                        _Logger.LogText(Payload);
+                                        throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                    }
+                                    break;
+                            }
+                            continue;
+                        }
+                        _Response = _HttpResponseMessage.Content.ReadAsStringAsync().Result;
+                        break;
+                    }
+                }
+            }
+            return Serializer.JsonSerializer.Deserialize<T>(_Response);
+        }
+        public string SendRequest(string EndPointUrl, string Action, string Payload, HttpMethod Method)
+        {
+            string _Response = string.Empty;
+            DateTime dt1 = DateTime.Now;
+            for (int i = 0; i < 4; i++)
+            {
+                System.Net.ServicePointManager.Expect100Continue = false;
+                System.Net.ServicePointManager.DefaultConnectionLimit = 9999;
+                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Ssl3;
+                using (HttpClient _HttpClient = new HttpClient())
+                using (HttpRequestMessage _HttpRequestMessage = new HttpRequestMessage(Method, EndPointUrl + Action))
+                {
+                    _HttpClient.DefaultRequestHeaders.Clear();
+                    _HttpClient.MaxResponseContentBufferSize = 2147483647;
+                    _HttpClient.Timeout = TimeSpan.FromMilliseconds(5400000);
+                    if (!string.IsNullOrEmpty(Payload)) _HttpRequestMessage.Content = new StringContent(Payload, Encoding.UTF8, "application/json");
+                    using (HttpResponseMessage _HttpResponseMessage = _HttpClient.SendAsync(_HttpRequestMessage).Result)
+                    {
+                        if (!_HttpResponseMessage.IsSuccessStatusCode)
+                        {
+                            switch (_HttpResponseMessage.StatusCode)
+                            {
+                                case System.Net.HttpStatusCode.BadRequest:
+                                    _Logger.LogText(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                    _Logger.LogText(EndPointUrl + Action);
+                                    _Logger.LogText(Payload);
+                                    throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                case System.Net.HttpStatusCode.Unauthorized:
+                                case System.Net.HttpStatusCode.NotFound:
+                                    throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                default:
+                                    TimeSpan span = (DateTime.Now) - dt1;
+                                    _Logger.LogText("Execution time: " + span.TotalMilliseconds.ToString() + " milliseconds, retried " + i.ToString() + " times.");
+                                    _Logger.LogText(EndPointUrl + Action);
+                                    _Logger.LogText(Payload);
+                                    if (i >= 3)
+                                    {
+                                        _Logger.LogText(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                        _Logger.LogText(EndPointUrl + Action);
+                                        _Logger.LogText(Payload);
+                                        throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                    }
+                                    break;
+                            }
+                            continue;
+                        }
+                        _Response = _HttpResponseMessage.Content.ReadAsStringAsync().Result;
+                        break;
+                    }
+                }
+            }
+            return _Response;
+        }
+        public LoginResponseDTO GetToken(string EndPointUrl, string Action, string UserName, string Password, HttpMethod Method)
+        {
+            string _Response = string.Empty;
+            DateTime dt1 = DateTime.Now;
+            System.Net.ServicePointManager.Expect100Continue = false;
+            System.Net.ServicePointManager.DefaultConnectionLimit = 9999;
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Ssl3;
+            var _formContent = new FormUrlEncodedContent(new[]
+            {
+                        new KeyValuePair<string, string>("grant_type", "password"),
+                        new KeyValuePair<string, string>("username", UserName),
+                        new KeyValuePair<string, string>("password", Password),
+            });
+            for (int i = 0; i < 4; i++)
+            {
+                using (HttpClient _HttpClient = new HttpClient())
+                using (HttpRequestMessage _HttpRequestMessage = new HttpRequestMessage(Method, EndPointUrl + Action))
+                {
+                    _HttpClient.DefaultRequestHeaders.Clear();
+                    _HttpClient.MaxResponseContentBufferSize = 2147483647;
+                    _HttpClient.Timeout = TimeSpan.FromMilliseconds(5400000);
+                    _HttpRequestMessage.Content = _formContent;
+                    using (HttpResponseMessage _HttpResponseMessage = _HttpClient.SendAsync(_HttpRequestMessage).Result)
+                    {
+                        if (!_HttpResponseMessage.IsSuccessStatusCode)
+                        {
+                            switch (_HttpResponseMessage.StatusCode)
+                            {
+                                case System.Net.HttpStatusCode.BadRequest:
+                                case System.Net.HttpStatusCode.Unauthorized:
+                                case System.Net.HttpStatusCode.NotFound:
+                                    throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                default:
+                                    TimeSpan span = (DateTime.Now) - dt1;
+                                    _Logger.LogText("Execution time: " + span.TotalMilliseconds.ToString() + " milliseconds, retried " + i.ToString() + " times.");
+                                    _Logger.LogText(EndPointUrl + Action);
+                                    if (i >= 3)
+                                    {
+                                        _Logger.LogText(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                        _Logger.LogText(EndPointUrl + Action);
+                                        throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + _HttpResponseMessage.Content.ReadAsStringAsync().Result);
+                                    }
+                                    break;
+                            }
+                            continue;
+                        }
+                        _Response = _HttpResponseMessage.Content.ReadAsStringAsync().Result;
+                        break;
+                    }
+                }
+            }
+            return Serializer.JsonSerializer.Deserialize<LoginResponseDTO>(_Response);
+        }
+        public async Task<T> SendRequestAsync<T>(string EndPointUrl, string Token, string Action, string Payload, HttpMethod Method)
+        {
+            DateTime dt1 = DateTime.Now;
+            string _Response = string.Empty;
+            for (int i = 0; i < 4; i++)
+            {
+                System.Net.ServicePointManager.Expect100Continue = false;
+                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Ssl3;
+                using (HttpClient _HttpClient = new HttpClient())
+                using (HttpRequestMessage _HttpRequestMessage = new HttpRequestMessage(Method, EndPointUrl + Action))
+                {
+                    _HttpClient.DefaultRequestHeaders.Clear();
+                    _HttpClient.MaxResponseContentBufferSize = 2147483647;
+                    _HttpClient.Timeout = TimeSpan.FromMilliseconds(5400000);
+                    _HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+                    _HttpRequestMessage.Content = new StringContent(Payload, Encoding.UTF8, "application/json");
+                    using (HttpResponseMessage _HttpResponseMessage = await _HttpClient.SendAsync(_HttpRequestMessage))
+                    {
+                        if (!_HttpResponseMessage.IsSuccessStatusCode)
+                        {
+                            switch (_HttpResponseMessage.StatusCode)
+                            {
+                                case System.Net.HttpStatusCode.BadRequest:
+                                    _Logger.LogText(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + await _HttpResponseMessage.Content.ReadAsStringAsync());
+                                    _Logger.LogText(EndPointUrl + Action);
+                                    _Logger.LogText(Payload);
+                                    throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + await _HttpResponseMessage.Content.ReadAsStringAsync());
+                                case System.Net.HttpStatusCode.Unauthorized:
+                                case System.Net.HttpStatusCode.NotFound:
+                                    throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + await _HttpResponseMessage.Content.ReadAsStringAsync());
+                                default:
+                                    TimeSpan span = (DateTime.Now) - dt1;
+                                    _Logger.LogText("Execution time " + span.TotalMilliseconds.ToString() + " milliseconds, retried " + i.ToString() + " times.");// ms.ToString("#,##0.00") + " minutes, retried " + i.ToString("0") + " times");
+                                    _Logger.LogText(EndPointUrl + Action);
+                                    _Logger.LogText(Payload);
+                                    if (i >= 3)
+                                    {
+                                        _Logger.LogText(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + await _HttpResponseMessage.Content.ReadAsStringAsync());
+                                        _Logger.LogText(EndPointUrl + Action);
+                                        _Logger.LogText(Payload);
+                                        throw new Exception(((int)_HttpResponseMessage.StatusCode).ToString() + " " + _HttpResponseMessage.ReasonPhrase + " " + await _HttpResponseMessage.Content.ReadAsStringAsync());
+                                    }
+                                    break;
+                            }
+                            continue;
+                        }
+                        _Response = _HttpResponseMessage.Content.ReadAsStringAsync().Result;
+                        break;
+                    }
+                }
+            }
+            return Serializer.JsonSerializer.Deserialize<T>(_Response);
+        }
+       
+    }
+}
---
+++ Trax.Framework.Generic/Serializer/JsonSerializer.cs
@@ -0,0 +1,89 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Trax.Framework.Generic.Serializer
+{
+    public class JsonSerializer
+    {
+        public static T UnserializeFile<T>(string FilePath)
+        {
+            if (!System.IO.File.Exists(FilePath))
+                throw new Exception("File not found");
+
+            T genericObject;
+            using (StreamReader file = File.OpenText(FilePath))
+            {
+                Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                //{
+                //    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                //};
+                genericObject = (T)serializer.Deserialize(file, typeof(T));
+            }
+            return genericObject;
+        }
+
+        public static T Deserialize<T>(string SerializedString)
+        {
+            T o = JsonConvert.DeserializeObject<T>(SerializedString);
+            return o;
+        }
+
+        public static void SerializeToFile<T>(T ObjectToSerialize, string FilePath)
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(FilePath)))
+                throw new Exception("FilePath directory not found");
+
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            var _MemoryStream = new MemoryStream();
+            serializer.WriteObject(_MemoryStream, ObjectToSerialize);
+            var JsonTradeItem = Encoding.UTF8.GetString(_MemoryStream.ToArray());
+            if (File.Exists(FilePath))
+                File.Delete(FilePath);
+
+            File.WriteAllText(FilePath, JsonTradeItem, Encoding.UTF8);
+        }
+
+        public static string Serialize<T>(T ObjectToSerialize)
+        {
+            //var serializer = new DataContractJsonSerializer(typeof(T));
+            //var _MemoryStream = new MemoryStream();
+            //serializer.WriteObject(_MemoryStream, ObjectToSerialize);
+            //var JsonTradeItem = Encoding.UTF8.GetString(_MemoryStream.ToArray());
+            //return JsonTradeItem;
+            return JsonConvert.SerializeObject(ObjectToSerialize, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+        }
+
+        public static string SerializeXML<T>(T Object)
+        {
+            string _Payload = string.Empty;
+            var _XmlSerializer = new XmlSerializer(typeof(T));
+            using (StringWriter _StringWriter = new Utf8StringWriter())
+            {
+                _XmlSerializer.Serialize(_StringWriter, Object);
+                _Payload = _StringWriter.ToString();
+            }
+            return _Payload;
+        }
+
+        public static T DesserializeXML<T>(string ObjectString)
+        {
+            T _Object = default(T);
+            var _XmlSerializer = new XmlSerializer(typeof(T));
+            using (MemoryStream _MemoryStream = new MemoryStream(Encoding.UTF8.GetBytes(ObjectString)))
+            {
+                _Object = (T)_XmlSerializer.Deserialize(_MemoryStream);
+            }
+            return _Object;
+        }
+
+        public class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;
+        }
+    }
+}
